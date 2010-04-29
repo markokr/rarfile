a95b2e791f5821769b9b24244af238785006fba8
Marko Kreen <markokr@gmail.com>
2010-04-29 23:27:54 +0300

    v2.0

--- setup.py
+++ setup.py
@@ -2,25 +2,26 @@
 
 from distutils.core import setup
 
-longdesc = """\
+longdesc = """
 This is Python module for Rar archive reading.  The interface
 is made as `zipfile` like as possible.
 
 The archive structure parsing and uncompressed files
-are handled in pure python, for compressed files
-the 'unrar' command line utility is called for data
-extraction.
+are handled in pure Python.  Decompression is done
+via 'unrar' command line utility.
 
 Features:
 
+- Works with both Python 2.x and 3.x
 - Supports RAR 3.x archives.
 - Supports multi volume archives.
 - Supports Unicode filenames.
+- Supports password-protected archives.
+- Supports archive comments.
 
 Missing features:
 
-- Password-protected archives.
-- Comment handling. (archive, file)
+- File comment handling.
 - Decompression through unrarlib and/or unrar.dll.
 
 """
@@ -33,7 +34,7 @@ setup(
     license = "ISC",
     author_email = "markokr@gmail.com",
     url = "http://rarfile.berlios.de/",
-    long_description = longdesc,
+    long_description = longdesc.strip(),
     py_modules = ['rarfile'],
     keywords = ['rar', 'archive'],
     classifiers = [
@@ -41,9 +42,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: ISC License (ISCL)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Archiving",
+        "Topic :: System :: Archiving :: Compression",
     ]
 )
 
