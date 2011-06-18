1e51b6c30ab5cc973f303dc0eae98fafa19c8929
Marko Kreen <markokr@gmail.com>
2011-06-18 21:47:55 +0300

    test2: check if python exe is actually available

--- test/test2.sh
+++ test/test2.sh
@@ -5,9 +5,11 @@ cp ../rarfile.py .
 plist="python2.4 python2.5 python2.6 python2.7 python3.1 python3.2 pypy jython"
 
 for py in $plist; do
-  echo "== $py =="
-  $py ./testseek.py
-  $py ./testio.py
+  if which $py > /dev/null; then
+    echo "== $py =="
+    $py ./testseek.py
+    $py ./testio.py
+  fi
 done
 
 rm -f rarfile.py
