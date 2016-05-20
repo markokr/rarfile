a8ebfbc791ea8458d7a2dd2d956db203ce0daaf0
Marko Kreen <markokr@gmail.com>
2016-05-20 16:18:11 +0300

    Remove old pythons from test

--- test/test2.sh
+++ test/test2.sh
@@ -4,7 +4,7 @@ cp ../rarfile.py .
 
 #ulimit -n 16
 
-plist="python2.4 python2.5 python2.6 python2.7 python3.1 python3.2 python3.3 python3.4 pypy jython jython2.7"
+plist="python2.6 python2.7 python3.2 python3.3 python3.4 python3.5 python3.6 pypy jython jython2.7"
 
 for py in $plist; do
   if which $py > /dev/null; then
