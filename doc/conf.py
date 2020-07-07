f2e9b15a1bde378052a3f1a7aa58335e35ec788a
Marko Kreen <markokr@gmail.com>
2020-07-08 01:18:31 +0300

    doc: try again

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'RarFile'
-copyright = None
+copyright = u''
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
