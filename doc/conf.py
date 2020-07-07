f1875aeb42e5ddd65ba62a62431bfdb2a1c97a2f
Marko Kreen <markokr@gmail.com>
2020-07-08 01:17:27 +0300

    doc: drop copyright, it messes style up

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'RarFile'
-copyright = u'2005-2019, Marko Kreen'
+copyright = None
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
