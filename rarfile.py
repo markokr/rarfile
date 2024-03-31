b6f7a4dfb3e15ae2d26fd7c5fc33d1a69dfa985c
Marko Kreen <markokr@gmail.com>
2024-04-01 01:14:33 +0300

    Update copyright

--- rarfile.py
+++ rarfile.py
@@ -1,6 +1,6 @@
 # rarfile.py
 #
-# Copyright (c) 2005-2020  Marko Kreen <markokr@gmail.com>
+# Copyright (c) 2005-2024  Marko Kreen <markokr@gmail.com>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
