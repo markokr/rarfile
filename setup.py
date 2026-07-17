60df9f63b71458af98776ef2fde36baac0f5199c
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2026-07-17 19:02:45 +0100

    Pass readme file and static description via pyproject.toml

--- setup.py
+++ setup.py
@@ -6,22 +6,10 @@ import sysconfig
 
 from setuptools import setup, Extension
 
-def description_short():
-    with open("README.rst") as readme:
-        ldesc = readme.read().strip()
-        sdesc = ldesc.split('\n')[0].split(' - ')[1].strip()
-        return sdesc
-
-def description_long():
-    with open("README.rst") as readme:
-        return readme.read().strip()
-
 # Free-threaded builds have no stable ABI yet (PEP 803)
 limited = "ABI3" in os.environ and not sysconfig.get_config_var("Py_GIL_DISABLED")
 
 setup(
-    description=description_short(),
-    long_description=description_long(),
     ext_modules=[
         Extension(
             name="rarfile._rarfile",
