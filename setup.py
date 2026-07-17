21edfcf27ff61961abeb12df9d347f22cc851577
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2026-07-17 19:19:08 +0100

    Limited builds if CIBUILDWHEEL and >=3.11 and not free-threaded

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-"""Setup script for rarfile.
-"""
+"""Setup script for rarfile."""
 
 import os
+import sys
 import sysconfig
 
 from setuptools import setup, Extension
 
-# Free-threaded builds have no stable ABI yet (PEP 803)
-limited = "ABI3" in os.environ and not sysconfig.get_config_var("Py_GIL_DISABLED")
+# Only the CI built wheels use the limited ABI
+# It is only supported on Python 3.11 or later, and not for 3.14 Free-threaded builds.
+limited = (
+    os.environ.get("CIBUILDWHEEL", "0") == "1"
+    and sys.version_info >= (3, 11)
+    and not sysconfig.get_config_var("Py_GIL_DISABLED")
+)
 
 setup(
     ext_modules=[
@@ -15,9 +20,7 @@ setup(
             name="rarfile._rarfile",
             sources=["src/rarfile/rarfile.c"],
             py_limited_api=limited,
-            define_macros=[
-                ("Py_LIMITED_API", "0x030B0000")
-            ] if limited else [],
+            define_macros=[("Py_LIMITED_API", "0x030B0000")] if limited else [],
         ),
     ],
     options={"bdist_wheel": {"py_limited_api": "cp311"} if limited else {}},
