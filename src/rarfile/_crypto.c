57c99a9dbb50a2178c556849f57ac7a978881cf0
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2026-07-17 20:52:48 +0100

    Update copyright comments

--- src/rarfile/_crypto.c
+++ src/rarfile/_crypto.c
@@ -1,12 +1,12 @@
 /*
 * rarfile.py
-#
+*
 * Copyright (c) 2005-2026  Marko Kreen <markokr@gmail.com>
-#
+*
 * Permission to use, copy, modify, and/or distribute this software for any
 * purpose with or without fee is hereby granted, provided that the above
 * copyright notice and this permission notice appear in all copies.
-#
+*
 * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
